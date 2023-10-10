ce1184dc7176fcf39083fd93fd6b78166dff5bf0
Hynniee <147478307+Hynniee@users.noreply.github.com>
2023-10-10 16:06:00 +0700

    Add files via upload

--- TRAN VU NGOC HUYNH - 110123012 - BAI 8.cpp
+++ TRAN VU NGOC HUYNH - 110123012 - BAI 8.cpp
@@ -1,8 +1,27 @@
 #include <stdio.h>
 #include<math.h>
-int n;
+int i,n;
 int KTNT(int x);
-int TONGNT(int a[],int x);
+int TONGNT(int y);
+
+int KTNT(int x)
+{
+	int h=0;
+		for(int i=1;i <= n;i++)
+	 		if(n%i==0)
+	 			h++;
+		if(h==2)
+			return 1;
+		return 0;
+}
+int TONGNT(int y)
+{
+	int i,tong=0;
+	for (i=1;i<=n;i++)
+			if(KTNT(i)==1);
+				tong=tong+i;
+	return tong;
+}
 int main()
 {
 	do
@@ -11,17 +30,9 @@ int main()
 	scanf("%d",&n);
 	}
 	while(n<0 || n>20);
-	KTNT(n);
+	if(KTNT(i)==1)
+		printf("%d la so nguyen to",n);
+	else printf("%d khong la so nguyen to",n);
+	TONGNT(n);
+	printf("tong la %d", TONGNT(n));
 }
-int KTNT(int x)
-{
-	int h=0;
-		for(int i=1;i <= n;i++)
-	 		if(n%i==0)
-	 			h++;
-		if(h==2)
-			printf("n la so nguyen to\n");
-		else
-			printf("n khong la so nguyen to\n");
-}
-
